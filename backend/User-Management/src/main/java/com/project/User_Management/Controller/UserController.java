7c932cb75d9a0750f79932b04a07953b84775279
Akshay alave <akshayalave@gmail.com>
2025-04-03 18:27:31 +0530

    add and controller and exception handlers along with multiple services

---
+++ backend/User-Management/src/main/java/com/project/User_Management/Controller/UserController.java
@@ -0,0 +1,44 @@
+package com.project.User_Management.Controller;
+
+import com.project.User_Management.Model.User;
+import com.project.User_Management.Service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    UserService service;
+    @GetMapping
+    public List<User> getAllUsers(){
+        return service.getAllUsers();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody @Valid User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createUser(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        return ResponseEntity.ok(service.updateUser(id, userDetails));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        service.deleteUser(id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+
+
+}
